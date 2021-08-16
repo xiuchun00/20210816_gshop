9bd56f721dfb6f1cac315023f445ea92475a419a
liuxiuchun <1103438387@qq.com>
2021-08-16 11:25:59 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FotterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FotterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  name: 'App',
+  components:{
+    FotterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
